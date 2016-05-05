8901aea700377d2a9c84a227960eec670a689897
Nome Sobrenome <diegofarias@alunos.utfpr.edu.br>
2016-05-05 16:37:24 -0300

    implementados efeitos visuais

--- js/galeria.js
+++ js/galeria.js
@@ -1,12 +1,20 @@
 function mostraImagens(estilo) {
 	var estrutura = '';
-		for (indice in estilo) { 
-			estrutura += 
-				'<div class="col-lg-4 col-md-6 col-xs-12">'+
-	                '<a class="thumbnail" href="#">'+
-	                    '<img class="img-responsive" src="' + estilo[indice].foto + '" alt="">'+
-	                '</a>'+
-		        '</div>';
-		}
-	return estrutura;
+			for (indice in estilo) { 
+				estrutura += 
+		            '<li>'+
+		            	'<figure>'+
+		                	'<div><img class="img-responsive" src="' + estilo[indice].icone + '" alt=""><div>'+
+		                	'<figcaption class="informacoes">'+
+		                		'<img src="' + estilo[indice].logo + '" alt="' + estilo[indice].nome + '">'+
+		                		'<a type="button" class="btn btn-default botao" role="button" href="' + estilo[indice].programacao + '" target="_blank">Programação</a>'+
+		                		'<div class="btn-group" role="group">'+
+									'<a type="button" class="btn btn-default" role="button" href="' + estilo[indice].foto + '" target="_blank">Foto</a>'+
+									'<a type="button" class="btn btn-default" role="button" href="' + estilo[indice].mapa + '" target="_blank">Mapa</a>'+
+								'</div>'+
+		                	'</figcaption>'+
+		                '</figure>'+
+		            '</li>';
+			}
+	return document.write(estrutura);
 }
--- js/listaDeBaladas.js
+++ js/listaDeBaladas.js
@@ -1,60 +1,63 @@
-function Balada(nome, estilo,  mapa) {
+function Balada(nome, estilo, mapa, programacao) {
 	this.nome = nome;
 	this.estilo = estilo;
 	this.mapa = mapa;
+	this.programacao = programacao;
 	this.foto = 'img/' + this.estilo + '/' + this.nome.toLowerCase().replace(/\s/g,'') + '/foto.jpg';
-	/*var logo = this.logo; implementar!*/
+	this.logo = 'img/' + this.estilo + '/' + this.nome.toLowerCase().replace(/\s/g,'') + '/logo.jpg';
+	this.icone = 'img/' + this.estilo + '/icone.jpg';
 }
 
 var eletronica = [
 	new Balada(
 	'Vibe',
 	'eletronica',
-	'https://goo.gl/EDnWiP'),
+	'https://goo.gl/maps/6ioNaU33YnM2',
+	'Noite da Tequila 01/06/2016'),
 	new Balada(
 	'Danghai',
 	'eletronica',
-	'https://goo.gl/EDnWiP'),
+	'https://goo.gl/maps/JUHJcCp6gYS2',
+	'Noite da Tequila 01/06/2016'),
 	new Balada(
 	'Duc',
 	'eletronica',
-	'https://goo.gl/EDnWiP'),
-	new Balada(
-	'Viva',
-	'eletronica',
-	'https://goo.gl/EDnWip')
+	'https://goo.gl/maps/DaZQuUtQRyG2',
+	'Noite da Tequila 01/06/2016')
 ];
 
 var rock = [
 	new Balada(
 	'Crossroads',
 	'rock',
-	'https://goo.gl/EDnWiP'),
+	'https://goo.gl/maps/dvuaoPo2TX62',
+	'Noite da Tequila 01/06/2016'),
 	new Balada(
 	'Blood',
 	'rock',
-	'https://goo.gl/EDnWiP'),
+	'https://goo.gl/maps/k8AYQvMYawQ2',
+	'Noite da Tequila 01/06/2016'),
 	new Balada(
 	'Sheridans',
 	'rock',
-	'https://goo.gl/EDnWiP')
+	'https://goo.gl/maps/9MoTuqP2aF22',
+	'Noite da Tequila 01/06/2016')
 ];
 
 var sertanejo = [
 	new Balada(
 	'Woods',
 	'sertanejo',
-	'https://goo.gl/EDnWiP'),
+	'https://goo.gl/maps/eQYcn4vkG2s',
+	'Noite da Tequila 01/06/2016'),
 	new Balada(
 	'Shed',
 	'sertanejo',
-	'https://goo.gl/EDnWiP'),
+	'https://goo.gl/maps/ZLopQZgQLiD2',
+	'Noite da Tequila 01/06/2016'),
 	new Balada(
 	'Victoria Villa',
 	'sertanejo',
-	'https://goo.gl/EDnWiP'),
-	new Balada(
-	'Woods Samba',
-	'sertanejo',
-	'https://goo.gl/EDnWiP')
+	'https://goo.gl/maps/2UawmFQaLTN2',
+	'Noite da Tequila 01/06/2016')
 ];
