6ba0e55b817197ed7c31bc86ecd84a267a1251df
Nome Sobrenome <diegofarias@alunos.utfpr.edu.br>
2016-05-06 17:24:24 -0300

    Adicionado randomicidade e calculo de bonus

---
+++ js/funcoes.js
@@ -0,0 +1,77 @@
+/*Gera um número randômico de 0 a max-1. Utilizado gerar um índice randômico de uma lista, que começa em 0*/
+function numeroRandomico(max) {
+	return Math.floor(Math.random() * max)
+}
+
+/*Verifica se um valor consta em uma lista*/
+function naoConsta(valor, lista) {
+	if (lista.indexOf(valor) == -1) {
+		return true
+	} else {
+		return false;
+	}
+}
+
+function randomizaLista(lista) {
+	var listaRandomizada = [];
+	var valorAleatorio = '';
+
+	while(listaRandomizada.length < lista.length) {
+		valorAleatorio = lista[numeroRandomico(lista.length)];
+
+		if (naoConsta(valorAleatorio, listaRandomizada)) {
+			listaRandomizada.push(valorAleatorio);
+		} else {
+			continue;
+		}
+	}
+	return listaRandomizada;
+}
+
+function calculaBonus(balada) {
+	var bonus;
+	var posicao = parseInt(indice) + 1;
+	switch(posicao) {
+	    case 1:
+	        bonus = balada[indice].bonusMaximo;
+	        break;
+	    case 2:
+	        bonus = balada[indice].bonusMaximo * 0.8;
+	        break;
+       	case 3:
+	        bonus = balada[indice].bonusMaximo * 0.6;
+	        break;
+	    default:
+	        bonus = balada[indice].bonusMaximo * 0.5;
+	}
+	return bonus;
+}
+
+function mostraBonus(bonus) {
+	alert('Parabéns, você recebeu ' + bonus + '% de desconto!');
+}
+
+function mostraGaleria(estilo) {
+	estilo = randomizaLista(estilo);
+	var estrutura = '';
+	var bonus;
+			for (indice in estilo) {
+				bonus = calculaBonus(estilo);
+				estrutura += 
+		            '<li>'+
+		            	'<figure>'+
+		                	'<div><img class="img-responsive" src="' + estilo[indice].icone + '" alt=""><div>'+
+		                	'<figcaption class="informacoes">'+
+		                		'<img src="' + estilo[indice].logo + '" alt="' + estilo[indice].nome + '">'+
+		                		'<a type="button" class="btn btn-default botao" role="button" href="' + estilo[indice].mapa + '" target="_blank">Informações</a>'+
+		                		'<div class="btn-group" role="group">'+
+									'<a type="button" class="btn btn-default" role="button" href="' + estilo[indice].foto + '" target="_blank">Foto</a>'+
+									'<a type="button" class="btn btn-default" role="button" href="#" onClick="mostraBonus(' + bonus + ')">Bônus</a>'+
+								'</div>'+
+		                	'</figcaption>'+
+		                '</figure>'+
+		            '</li>';
+			}
+	return document.write(estrutura);
+}
+
--- js/listaDeBaladas.js
+++ js/listaDeBaladas.js
@@ -1,8 +1,9 @@
-function Balada(nome, estilo, mapa, programacao) {
+function Balada(nome, estilo, mapa, programacao, bonusMaximo) {
 	this.nome = nome;
 	this.estilo = estilo;
 	this.mapa = mapa;
 	this.programacao = programacao;
+	this.bonusMaximo = bonusMaximo;
 	this.foto = 'img/' + this.estilo + '/' + this.nome.toLowerCase().replace(/\s/g,'') + '/foto.jpg';
 	this.logo = 'img/' + this.estilo + '/' + this.nome.toLowerCase().replace(/\s/g,'') + '/logo.jpg';
 	this.icone = 'img/' + this.estilo + '/icone.jpg';
@@ -13,17 +14,20 @@ var eletronica = [
 	'Vibe',
 	'eletronica',
 	'https://goo.gl/maps/6ioNaU33YnM2',
-	'Noite da Tequila 01/06/2016'),
+	'Noite da Tequila 01/06/2016',
+	10),
 	new Balada(
 	'Danghai',
 	'eletronica',
 	'https://goo.gl/maps/JUHJcCp6gYS2',
-	'Noite da balalaika 01/06/2016'),
+	'Noite da balalaika 01/06/2016',
+	15),
 	new Balada(
 	'Duc',
 	'eletronica',
 	'https://goo.gl/maps/DaZQuUtQRyG2',
-	'Noite da Tequila 01/06/2016')
+	'Noite da Tequila 01/06/2016',
+	5)
 ];
 
 var rock = [
@@ -31,17 +35,20 @@ var rock = [
 	'Crossroads',
 	'rock',
 	'https://goo.gl/maps/dvuaoPo2TX62',
-	'Noite da Tequila 01/06/2016'),
+	'Noite da Tequila 01/06/2016',
+	20),
 	new Balada(
 	'Blood',
 	'rock',
 	'https://goo.gl/maps/k8AYQvMYawQ2',
-	'Noite da Tequila 01/06/2016'),
+	'Noite da Tequila 01/06/2016',
+	3),
 	new Balada(
 	'Sheridans',
 	'rock',
 	'https://goo.gl/maps/9MoTuqP2aF22',
-	'Noite da Tequila 01/06/2016')
+	'Noite da Tequila 01/06/2016',
+	8)
 ];
 
 var sertanejo = [
@@ -49,15 +56,20 @@ var sertanejo = [
 	'Woods',
 	'sertanejo',
 	'https://goo.gl/maps/eQYcn4vkG2s',
-	'Noite da Tequila 01/06/2016'),
+	'Noite da Tequila 01/06/2016',
+	10),
 	new Balada(
 	'Shed',
 	'sertanejo',
 	'https://goo.gl/maps/ZLopQZgQLiD2',
-	'Noite da Tequila 01/06/2016'),
+	'Noite da Tequila 01/06/2016',
+	12),
 	new Balada(
 	'Victoria Villa',
 	'sertanejo',
 	'https://goo.gl/maps/2UawmFQaLTN2',
-	'Noite da Tequila 01/06/2016')
+	'Noite da Tequila 01/06/2016',
+	6)
 ];
+
+var todos = eletronica.concat(rock, sertanejo);
